250eed64f73fc3a2d7240a0ca77add9f8bd3c960
brijeshdonga113 <prijeshdonga113@gmail.com>
2025-06-29 22:29:02 +0530

    solution

--- src/main/java/com/example/LongestString.java
+++ src/main/java/com/example/LongestString.java
@@ -1,4 +1,30 @@
 package com.example;
 
 public class LongestString {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        int leftPointer = 0, rightPointer = 0;
+        int maxLen = 0;
+
+        HashSet<Character> hashSet = new HashSet<>();
+
+        while (rightPointer < s.length()) {
+            char currentChar = s.charAt(rightPointer);
+
+            //check if char is present in the set
+            if (!hashSet.contains(currentChar)) {
+                hashSet.add(currentChar);
+                maxLen = Math.max(maxLen, rightPointer - leftPointer + 1);
+                rightPointer++;
+            } else {
+                hashSet.remove(s.charAt(leftPointer));
+                leftPointer++;
+            }
+        }
+
+        return maxLen;
+
+    }
+
 }
